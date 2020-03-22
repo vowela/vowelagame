297155ab1deadea463e1c5f76494d24d8a1c412a
Stanislav <sgkryvenko@gmail.com>
2020-03-22 22:45:32 +0700

    Added Lua Framework (MoonSharp)
    https://www.moonsharp.org/getting_started.html

---
+++ Client/VowelA/Assets/Scripts/Real-Time/RealTimeCompiler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoonSharp.Interpreter;
+
+public class RealTimeCompiler : MonoBehaviour
+{
+    
+    public bool IsCompileScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    double MoonSharpFactorial() {
+        string script = @"
+        --defines a factorial function
+        function fact(n) 
+            if (n == 0) then
+                return 1
+            else 
+                return n*fact(n-1)
+            end
+        end
+        
+        print(fact(5))";
+
+        var res = Script.RunString(script);
+        return res.Number;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsCompileScript) {
+            Debug.Log("Compiled func: " + MoonSharpFactorial());
+            IsCompileScript = false;
+        }
+    }
+}

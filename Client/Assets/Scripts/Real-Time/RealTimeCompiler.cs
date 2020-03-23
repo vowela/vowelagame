b31945bbed949edafa7a5ebfaa620a6fdac8815b
Stanislav <sgkryvenko@gmail.com>
2020-03-23 20:32:49 +0700

    upd

--- Client/Assets/Scripts/Real-Time/RealTimeCompiler.cs
+++ Client/Assets/Scripts/Real-Time/RealTimeCompiler.cs
@@ -2,40 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using MoonSharp.Interpreter;
+using VowelAServer.Shared.Data;
 
 public class RealTimeCompiler : MonoBehaviour
 {
-    
-    public bool IsCompileScript;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    double MoonSharpFactorial() {
-        string script = @"
-        --defines a factorial function
-        function fact(n) 
-            if (n == 0) then
-                return 1
-            else 
-                return n*fact(n-1)
-            end
-        end
+    static void CompileData(ContainerObject container) {
         
-        print(fact(5))";
-
-        var res = Script.RunString(script);
-        return res.Number;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (IsCompileScript) {
-            Debug.Log("Compiled func: " + MoonSharpFactorial());
-            IsCompileScript = false;
-        }
     }
 }

7b93e242e7880a9de2c7d6cb2a2b5d8d643b8da3
Stanislav <sgkryvenko@gmail.com>
2020-03-23 20:10:03 +0700

    MoonSharp added, setup for serv project

---
+++ Server/VowelAServer.Server/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using VowelAServer.Shared.Data;
+
+namespace VowelAServer.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var customObject = new ContainerObject();
+        }
+    }
+}

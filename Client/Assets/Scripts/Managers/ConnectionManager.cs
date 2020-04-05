9d146b2cec1a3858d77a60528a5083df20aae0e2
Nikita Mingaleev <nmingaleev@yandex.ru>
2020-04-05 13:22:17 +0700

    Added reconnect to Client

--- Client/Assets/Scripts/Managers/ConnectionManager.cs
+++ Client/Assets/Scripts/Managers/ConnectionManager.cs
@@ -14,7 +14,10 @@ public class ConnectionManager : MonoBehaviour
     public static Host Client;
     public string Ip = "127.0.0.1";
     public ushort Port = 6005;
+    public Address address;
     private int skipFrame = 0;
+
+    public bool tryConnect = true;
 
     void Awake ()
     {
@@ -43,14 +46,14 @@ public class ConnectionManager : MonoBehaviour
     {
         ENet.Library.Initialize();
         Client = new Host();
-        Address address = new Address();
+        address = new Address();
 
         address.SetHost(Ip);
         address.Port = Port;
         Client.Create();
         
         Debug.Log("Connecting");
-        CurrentPeer = Client.Connect(address);
+        Connect();
     }
 
     private void UpdateENet()
@@ -80,10 +83,14 @@ public class ConnectionManager : MonoBehaviour
 
             case ENet.EventType.Disconnect:
                 Debug.Log("Client disconnected from server");
+
+                Connect();
                 break;
 
             case ENet.EventType.Timeout:
                 Debug.Log("Client connection timeout");
+
+                Connect();
                 break;
 
             case ENet.EventType.Receive:
@@ -106,5 +113,12 @@ public class ConnectionManager : MonoBehaviour
 
         if (netEvent.Type == ENet.EventType.Receive)
             netEvent.Packet.Dispose();
+    }
+
+    private void Connect() {
+        if (!tryConnect)
+            return;
+
+        CurrentPeer = Client.Connect(address);
     }
 }

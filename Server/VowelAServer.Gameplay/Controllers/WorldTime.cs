432778cc2864d1da276af12218fdd61ae04f9948
Nikita Mingaleev <nmingaleev@yandex.ru>
2020-05-06 23:12:38 +0700

    Added world time saving

--- Server/VowelAServer.Gameplay/Controllers/WorldTime.cs
+++ Server/VowelAServer.Gameplay/Controllers/WorldTime.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Diagnostics;
+using System.IO;
+using VowelAServer.Shared.Utils;
 
 namespace VowelAServer.Gameplay.Controllers
 {
@@ -7,6 +10,21 @@ namespace VowelAServer.Gameplay.Controllers
     {
         private static WorldTime instance;
         private static object syncRoot = new object();
+
+        private static string timeDataPath;
+
+        public static string TimeDataPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(timeDataPath))
+                {
+                    timeDataPath = Utils.GetDirPath("Storage") + @"/time.json";
+                }
+
+                return timeDataPath;
+            }
+        }
 
         public static WorldTime Instance()
         {
@@ -22,10 +40,23 @@ namespace VowelAServer.Gameplay.Controllers
 
         private Stopwatch stopwatch;
 
+        private DateTime startTime;
+
         public WorldTime()
         {
             stopwatch = new Stopwatch();
+        }
+
+        public void Start(DateTime dateTime)
+        {
+            startTime = dateTime;
+
             stopwatch.Start();
+        }
+
+        public DateTime GetCurrentTime()
+        {
+            return startTime.AddMilliseconds(stopwatch.ElapsedMilliseconds);
         }
 
         // Milliseconds
@@ -33,5 +64,50 @@ namespace VowelAServer.Gameplay.Controllers
         {
             return stopwatch.ElapsedMilliseconds;
         }
+
+        public float GetWorldTimeSeconds()
+        {
+            return GetWorldTime() / 1000;
+        }
+
+        public float GetWorldTimeMinutes()
+        {
+            return GetWorldTimeSeconds() / 60;
+        }
+
+        public float GetWorldTimeHours()
+        {
+            return GetWorldTimeMinutes() / 60;
+        }
+
+        public float GetWorldTimeDays()
+        {
+            return GetWorldTimeHours() / 24;
+        }
+
+        public void SaveTime()
+        {
+            var time = new Time()
+            {
+                Ticks = GetCurrentTime().Ticks,
+            };
+
+            File.WriteAllText(TimeDataPath, JsonConvert.SerializeObject(time));
+        }
+
+        public static DateTime GetSavedTime()
+        {
+            var file = File.ReadAllText(TimeDataPath);
+            if (string.IsNullOrEmpty(file)) return DateTime.Now;
+
+            var time = JsonConvert.DeserializeObject<Time>(file);
+
+            return new DateTime(time.Ticks);
+        }
+    }
+
+    class Time
+    {
+        public long Ticks;
     }
 }

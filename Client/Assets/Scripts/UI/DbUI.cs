7fce76575f143de59a326e1d0a416934708791f3
Nikita Mingaleev <nmingaleev@yandex.ru>
2020-10-22 02:00:23 +0700

    Try table render

--- Client/Assets/Scripts/UI/DbUI.cs
+++ Client/Assets/Scripts/UI/DbUI.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using VowelAServer.Shared.Models.Db;
@@ -7,6 +10,11 @@ using VowelAServer.Shared.Models.Db;
 public class DbUI : MonoBehaviour
 {
     public Transform CollectionsList;
+    public Transform TabelPanelContent;
+
+    public GameObject HeaderCell;
+    public GameObject BodyCell;
+    public GameObject TableRow;
     
     // Start is called before the first frame update
     void Start()
@@ -17,17 +25,10 @@ public class DbUI : MonoBehaviour
         DbController.RequestCollectionNames();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void RenderCollections(IEnumerable<string> collectionNames)
     {
         foreach (var collectionName in collectionNames)
         {
-            Debug.Log(collectionName);
             var button = DefaultControls.CreateButton(new DefaultControls.Resources());
 
             button.GetComponentInChildren<Text>().text = collectionName;
@@ -39,6 +40,57 @@ public class DbUI : MonoBehaviour
 
     private void RenderCollectionItems(IEnumerable<Row> rows)
     {
-        // TODO: render table
+        ClearTable();
+        
+        var firstRow = rows.First();
+        if (firstRow == null) return;
+
+        RenderTableHeader(firstRow);
+        RenderBody(rows);
+    }
+
+    private void RenderTableHeader(Row header)
+    {
+        var headerRow = Instantiate(TableRow);
+
+        foreach (var item in header.Items)
+        {
+            RenderCell(HeaderCell, item.Key, headerRow.transform);
+            
+            headerRow.transform.SetParent(TabelPanelContent, false);
+        }
+    }
+
+    private void RenderBody(IEnumerable<Row> rows)
+    {
+        foreach (var row in rows)
+        {
+            var tableRow = Instantiate(TableRow);
+            
+            foreach (var item in row.Items)
+            {
+                RenderCell(BodyCell, item.Value, tableRow.transform);
+            
+                tableRow.transform.SetParent(TabelPanelContent, false);
+            }
+        }
+    }
+
+    private void ClearTable()
+    {
+        for (var i = TabelPanelContent.childCount - 1; i >= 0; i--)
+        {
+            Destroy(TabelPanelContent.GetChild(i).gameObject);
+        }
+    }
+
+    private void RenderCell(GameObject cellPrefab, String value, Transform parent)
+    {
+        var cell = Instantiate(cellPrefab);
+                
+        var cellText = cell.gameObject.GetComponent<TextMeshProUGUI>();
+        cellText.SetText(value);
+                
+        cell.transform.SetParent(parent, false);
     }
 }
